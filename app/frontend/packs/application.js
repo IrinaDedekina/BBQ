6f2172c9f4a58513a5dc778d8da69d9eabd7c631
IrinaDedekina <dedekina92@yandex.ru>
2021-03-20 20:21:15 +0300

    add map for events

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -5,5 +5,6 @@ Rails.start()
 import "bootstrap/dist/js/bootstrap"
 import "../styles/application"
 import "bootstrap-icons/font/bootstrap-icons"
+import "../scripts/map"
 
 const images = require.context("../images", true)
