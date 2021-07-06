e42139a0605e5b88944c070773b35a99a528e88f
IrinaDedekina <dedekina92@yandex.ru>
2021-07-06 16:32:07 +0300

    Add pincode to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,8 @@ class EventsController < ApplicationController
 
   before_action :set_event, except: [:index, :new, :create]
 
+  before_action :password_guard!, only: [:show]
+
   # Задаем объект @event от текущего юзера для других действий
   after_action :verify_authorized, only: [:edit, :update, :destroy, :show]
 
@@ -64,11 +66,29 @@ class EventsController < ApplicationController
 
   private
 
+  def password_guard!
+    return true if @event.pincode.blank?
+    return true if signed_in? && current_user == @event.user
+
+    if params[:pincode].present? && @event.pincode_valid?(params[:pincode])
+      cookies.permanent["events_#{@event.id}_pincode"] = params[:pincode]
+    end
+
+    pincode = cookies.permanent["events_#{@event.id}_pincode"]
+    unless @event.pincode_valid?(pincode)
+      if params[:pincode].present?
+        flash.now[:alert] = I18n.t('controllers.events.wrong_pincode')
+      end
+      render 'password_form'
+    end
+  end
+
   def set_event
     @event = Event.find(params[:id])
   end
 
   def event_params
-    params.require(:event).permit(:title, :address, :datetime, :description)
+    params.require(:event)
+      .permit(:title, :address, :datetime, :description, :pincode)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_134543) do
+ActiveRecord::Schema.define(version: 2021_07_06_115612) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_02_06_134543) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
